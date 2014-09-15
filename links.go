d6f335df99074cb3f6a344c2e84c918d1a5e0da7
jimmyfrasche <soapboxcicero@gmail.com>
2014-09-14 21:56:25 -0700

    removed skipping javascript links

--- links.go
+++ links.go
@@ -1,19 +1,9 @@
 package main
 
-import "strings"
-
 type links map[string]struct{}
 
 func (l links) Add(src string) {
 	if src == "" {
-		return
-	}
-	//unobtrusive javascript link, ignore
-	if src == "#" {
-		return
-	}
-	//obtrusive javascript link, burn with fire
-	if strings.HasPrefix(strings.ToLower(src), "javscript:") {
 		return
 	}
 	l[src] = struct{}{}
